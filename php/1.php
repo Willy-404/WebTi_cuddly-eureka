b412eaeea0fdb1b27d0a1a03c267f948978f2ac7
Willian dos Anjos <165198824+Willy-404@users.noreply.github.com>
2025-06-18 08:45:26 -0300

    PHP

--- php/1.php
+++ php/1.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-     echo "ola mundao"
+<?php
+    echo "olá mundão perdido ";
+    $nome = "Romulo ";
+    echo gettype($nome). "<br>";
+    
+    echo $nome;
+    
+    $curso = $_GET["nome"];
+    echo $curso;
 
-
-
-
-
-
-
-
-
-
-
-    ?>
-</body>
-</html>
+    var_dump($_REQUEST)
+?>    
